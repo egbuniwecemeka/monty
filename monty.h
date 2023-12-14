7de13d2c59ce308d45a472b14ccfe012e2aae4f5
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-14 10:01:18 -0800

    betty update

--- monty.h
+++ monty.h
@@ -20,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,8 +35,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t *head;
@@ -46,7 +46,7 @@ typedef void (*op_func)(stack_t **, unsigned int);
 void open_file(*file_name);
 int parse_file(char *buffer, int line_number, int format);
 void read_file(FILE *);
-int len_chars(FILE *)l
+int len_chars(FILE *);
 void find_finc(char, char, int, int);
 
 /* Stack operations */
